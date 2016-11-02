20ca8b653f0593d8cdaf74db0d6c933f87e05895
JonasMalik <jonas14550@gmail.com>
2016-11-02 04:26:40 +0100

    spinner textColor changed

--- app/src/main/java/com/jonasmalik94/workhours/Controller/SpinnerListener.java
+++ app/src/main/java/com/jonasmalik94/workhours/Controller/SpinnerListener.java
@@ -1,6 +1,9 @@
 package com.jonasmalik94.workhours.Controller;
 
 import android.app.Activity;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.TextView;
@@ -13,10 +16,13 @@ import com.jonasmalik94.workhours.R;
 
 public class SpinnerListener extends Activity implements AdapterView.OnItemSelectedListener {
     // Spinner text color
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-       // ((TextView) adapterView.getChildAt(0)).setTextColor(view.getResources().getColor(R.color.text_gray));
-
+        TextView selectedText = (TextView) adapterView.getChildAt(0);
+        if (selectedText != null) {
+            selectedText.setTextColor(Color.GRAY);
+        }
     }
 
     @Override
